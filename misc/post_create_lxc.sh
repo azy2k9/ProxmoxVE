db39453ecf8d927b9f321200f08bf24a86438a8e
azy2k9 <azy2k9@gmail.com>
2026-01-23 11:57:29 +0000

    Switch from proxmox directory to zfs pool mapping

--- misc/post_create_lxc.sh
+++ misc/post_create_lxc.sh
@@ -88,7 +88,8 @@ if [[ "${SHARED_MOUNT}" == "yes" ]]; then
     pct exec $CTID -- /bin/bash -c "adduser $SHARE_USER --disabled-password --no-create-home --gecos '' --uid 1001 &>/dev/null"
     
     # Add mount point (Shared for both privileged and unprivileged)
-    echo "mp0: /mnt/pve/cctv,mp=/media/frigate" >> /etc/pve/lxc/${CTID}.conf
+    # echo "mp0: /mnt/pve/cctv,mp=/media/frigate" >> /etc/pve/lxc/${CTID}.conf - old way for proxmox directory
+    echo "mp0: cctv:subvol-102-disk-1,mp=/media/frigate,size=6000G" >> /etc/pve/lxc/${CTID}.conf
     
     # Check if container is UNPRIVILEGED (only unprivileged needs ID mapping)
     if grep -q "unprivileged: 1" /etc/pve/lxc/${CTID}.conf; then
